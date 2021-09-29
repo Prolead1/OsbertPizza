b678e4579131fa0af7e866a78912bd4571671439
Prolead1 <retroearthgeek1@gmail.com>
2021-09-29 22:02:13 +0800

    completed login and register

--- hero.js
+++ hero.js
@@ -7,8 +7,8 @@ slideshows.forEach(initSlideShow);
 function initSlideShow(slideshow) {
 
 	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
-
-	var index = 0, time = 5000;
+	var index = 0, time = 10000;
+    var timer = setInterval( autoNextSlide, time);
 	slides[index].classList.add('active');
 
     leftbutton.addEventListener('click', () => {
@@ -27,8 +27,6 @@ function initSlideShow(slideshow) {
         if (index === slides.length) index = 0;
         slides[index].classList.add('active');
     })
-    
-    var timer = setInterval( autoNextSlide, time);
     function autoNextSlide(){
 		slides[index].classList.remove('active');
 		
